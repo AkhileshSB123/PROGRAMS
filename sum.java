ff6e23822f760d4d19491b39d35d24e41d393f45
AkhileshSB <akhileshsb123@gmail.com>
2019-03-12 10:57:07 +0530

    version2

--- sum.java
+++ sum.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class sum{
 
     public static void main(String[] args)
     {
-        int a=10,b=20,c;
+        Scanner myinput =new Scanner(System.in);
+        int a,b,c;
+        
+        System.out.println("enter first no:");
+        a=myinput.nextInt();
+        System.out.println("enter second no:");
+        b=myinput.nextInt();
         c=a+b;
         System.out.println("sum="+c);
     }
